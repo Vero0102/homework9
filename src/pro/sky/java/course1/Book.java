caf97872864577515a80b1d02827f9b27c0cfbf2
Vero0102 <104280568+Vero0102@users.noreply.github.com>
2022-06-13 19:20:38 +0300

    homework 1.9 is ended

---
+++ src/pro/sky/java/course1/Book.java
@@ -0,0 +1,30 @@
+package pro.sky.java.course1;
+
+public class Book {
+    private final String title;
+    private final Author author;
+    private int yearOfPublication;
+
+    public Book(String title, Author author, int yearOfPublication) {
+        this.title = title;
+        this.author = author;
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getYearOfPublication() {
+        return yearOfPublication;
+    }
+
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+}
+
