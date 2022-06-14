2ca53b00171310717d7e19ab41772d9561e8bbe3
Vero0102 <104280568+Vero0102@users.noreply.github.com>
2022-06-14 10:29:01 +0300

    Исправила

--- src/pro/sky/java/course1/Main.java
+++ src/pro/sky/java/course1/Main.java
@@ -3,18 +3,18 @@ package pro.sky.java.course1;
 public class Main {
 
     public static void main(String[] args) {
-        Author authorCardNumber1 = new Author("Габриэль", "Гарсиа", "Маркес", "", "", "");
-        Author authorCardNumber2 = new Author("Кэти", "", "Сьерра", "Берт", "", "Бэйтс");
+        Author authorCardNumber1 = new Author("Габриэль Гарсиа", "Маркес");
+        Author authorCardNumber2 = new Author("Кэти", "Сьерра");
 
         Book bookCardNumber1 = new Book("Сто лет одиночества", authorCardNumber1, 1967);
         Book bookCardNumber2 = new Book("Изучаем Java", authorCardNumber2, 2003);
 
-        System.out.println("Название книги: " + bookCardNumber1.getTitle() + ". Год издания: " + bookCardNumber1.getYearOfPublication() + ". Автор: " + bookCardNumber1.getAuthor().getSurname1() + " " + bookCardNumber1.getAuthor().getName1() + " " + bookCardNumber1.getAuthor().getMiddleName1() + ".");
-        System.out.println("Название книги: " + bookCardNumber2.getTitle() + ". Год издания: " + bookCardNumber2.getYearOfPublication() + ". Авторы: " + bookCardNumber2.getAuthor().getSurname1() + " " + bookCardNumber2.getAuthor().getName1() + " и " + bookCardNumber2.getAuthor().getSurname2() + " " + bookCardNumber2.getAuthor().getName2() + bookCardNumber2.getAuthor().getMiddleName2() + ".");
+        System.out.println("Название книги: " + bookCardNumber1.getTitle() + ". Год издания: " + bookCardNumber1.getYearOfPublication() + ". Автор: " + bookCardNumber1.getAuthor().getSurname() + " " + bookCardNumber1.getAuthor().getName() + ".");
+        System.out.println("Название книги: " + bookCardNumber2.getTitle() + ". Год издания: " + bookCardNumber2.getYearOfPublication() + ". Автор: " + bookCardNumber2.getAuthor().getSurname() + " " + bookCardNumber2.getAuthor().getName() + ".");
 
         bookCardNumber2.setYearOfPublication(2021);
 
-        System.out.println("Название книги: " + bookCardNumber2.getTitle() + ". Год издания: " + bookCardNumber2.getYearOfPublication() + ". Авторы: " + bookCardNumber2.getAuthor().getSurname1() + " " + bookCardNumber2.getAuthor().getName1() + " и " + bookCardNumber2.getAuthor().getSurname2() + " " + bookCardNumber2.getAuthor().getName2() + bookCardNumber2.getAuthor().getMiddleName2() + ".");
+        System.out.println("Название книги: " + bookCardNumber2.getTitle() + ". Год издания: " + bookCardNumber2.getYearOfPublication() + ". Автор: " + bookCardNumber2.getAuthor().getSurname() + " " + bookCardNumber2.getAuthor().getName() + ".");
     }
 }
 
